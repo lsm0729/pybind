d36f3ec90fd20bd3b133df8f2b3aa2dec0adba87
lsm0729 <lsm0729@yonsei.ac.kr>
2022-08-18 02:24:01 +0900

    uu

---
+++ Main/main.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<Python.h>
+#include<pybind11/embed.h>
+
+
+
+int main()
+{
+	pybind11::scoped_interpreter guard{};
+	auto sys = pybind11::module::import("sys");
+	pybind11::print(sys.attr("path"));
+	std::cout << 32324 << std::endl;
+	system("pause");
+	return 0;
+
+}
+
